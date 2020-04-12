951301287d16c1e66a4f0215ba1a901782c27b97
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-04-12 18:29:47 +0000

    Automatically backed up by Learn

--- 01_create_users.rb
+++ 01_create_users.rb
@@ -1,2 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.number :balance
+    end
+  end
 end
